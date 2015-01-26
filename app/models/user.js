3fa1b5bf906f6ad22e383898363ac9339c5d49d1
devin-liu & Vincent Colavin <pair+devin-liu.vcolavin@devbootcamp.com>
2015-01-26 10:24:53 -0800

    fix user model

--- app/models/user.js
+++ app/models/user.js
@@ -4,12 +4,8 @@ var Schema  = mongoose.Schema
 
 var UserSchema = new Schema({
   name: {type: String, required: true},
-  // FIXME: need to implement uniqueness validator for email
-  // Mongoose doesn't have one natively but there is a package
-  // for it
-  email: {type: String, required: true},
   email: {type: String, required: true, unique: true},
-  // city: {type: String, required: true}
+  city: {type: String, required: true}
 })
 
 UserSchema.plugin(uniqueValidator)
